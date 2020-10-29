2c806ca610b92b74153e3b8f2a270ce6b77be051
Pushkar Bhuse <pushkar_bhuse@Pushkars-MacBook-Pro.local>
2020-10-29 23:28:55 +0530

    Temp question excel reader

--- backend/api/routes/question/addQuestion.js
+++ backend/api/routes/question/addQuestion.js
@@ -1,10 +1,31 @@
 const { addQuestion } = require("../../dbFunctions/question");
 const { ServerError, Success } = require("../../responses");
+const xlsxFile = require('read-excel-file/node');
+
 module.exports = async (req, res) => {
     try {
-        const newquestion = await addQuestion(req.body.description, req.body.alternatives, req.body.subtopic, req.body.correct);
-        console.log(newquestion);
-        res.json({ ...Success });
+        var question_data = {};
+        xlsxFile('./Data.xlsx').then((rows) => {
+            rows.forEach((col)=>{
+                        question_data['question'] = cols[0];
+                        question_data['alternatives'] = []
+                        for(var i = 0; i < 4; i++){
+                            question_data['alternatives'].push(
+                                {
+                                    'text': cols[i+1],
+                                    'isCorrect': false
+                                }
+                            )
+                        }
+                        question_data['alternatives'][cols[4]]['isCorrect'] = true;
+                        question_data['subtopic'] =  col[5];
+                        question_data['image'] = col[6]
+            })
+        })
+
+        // const newquestion = await addQuestion(req.body.description, req.body.alternatives, req.body.subtopic, req.body.correct);
+        // console.log(newquestion);
+        // res.json({ ...Success });
     }
     catch (err) {
         console.log(err);
