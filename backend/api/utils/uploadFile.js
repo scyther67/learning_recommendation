072ce8aefae0b764c2c986e70ffec673e5c2b56d
Jash <scyther67@gmail.com>
2020-10-31 00:13:00 +0530

    Multer image upload done

--- backend/api/utils/uploadFile.js
+++ backend/api/utils/uploadFile.js
@@ -1,25 +1,25 @@
 const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         console.log(process.cwd() + './temp/');
-//         cb(null, process.cwd() + '/temp');
-//     },
-//     filename: function (req, file, cb) {
-//         req.body[file.fieldname] = file.originalname;
-//         cb(null, file.originalname);
-//     }
-// });
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
+    destination: function (req, file, cb) {
+        console.log(process.cwd()+'\\api\\utils\\question_images_temp');
+        cb(null, process.cwd() + '\\api\\utils\\question_images_temp');
     },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname));
+    filename: function (req, file, cb) {
+        req.body[file.fieldname] = file.originalname;
+        cb(null, Date.now()+file.originalname);
     }
 });
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads');
+//     },
+//     filename: (req, file, cb) => {
+//         console.log(file);
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     }
+// });
 
 // const fileFilter = (req, file, cb) => {
 //     if (file.mimetype == 'image/*' || file.mimetype == 'image/png') {
