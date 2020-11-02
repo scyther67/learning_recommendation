21f86fcd3fcf14996df9f20e02ef41f0bcca71d9
Abit25 <abheet25@gmail.com>
2020-11-02 22:50:41 +0530

    Single Question Added

--- frontend/shards-dashboard-react/src/views/ViewQuestion.js
+++ frontend/shards-dashboard-react/src/views/ViewQuestion.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "shards-react";
 import Options from "../components/answers/Options";
-import data from "../data/questions";
+// import data from "../data/questions";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 
@@ -36,6 +36,7 @@ const ViewQuestion = () => {
       history.push("/sign-in");
     }
   }, []);
+  const [data, setData] = useState([]);
   const [nr, setNr] = useState(0);
   const [total, setTotal] = useState(data.length);
   const [showButton, setShowButton] = useState(false);
@@ -46,8 +47,30 @@ const ViewQuestion = () => {
   const [timestamps, setTS] = useState([]);
   const [responses, setRP] = useState([]);
   const [QId, setQid] = useState(0);
+  const [student_response_id, setSRId] = useState("");
 
-  useEffect(() => {
+  useEffect(async () => {
+    //request first question
+    try {
+      const config = {
+        headers: {
+          Authorization: localStorage.getItem("user_token")
+        }
+      };
+      const res = await axios.post(
+        "http://localhost:5000/api/question/reqQuestion",
+        { question_no: 0 },
+        config
+      );
+      console.log(res.status);
+      //add question to data array
+      let newData = data;
+      setSRId(res.data.student_response_id);
+      newData.append(res.data.random_question);
+      setData(newData);
+    } catch (error) {
+      console.log(error);
+    }
     pushData(nr);
   }, []);
 
@@ -58,40 +81,78 @@ const ViewQuestion = () => {
 
   const nextQuestion = async e => {
     if (nr != total) {
-      setNr(nr + 1);
-      pushData(nr);
-      setShowButton(false);
-      setQA(false);
-    } else {
-      let modified_tp = timestamps;
-      let start_time = await localStorage.getItem("start_time");
-      modified_tp[0]["start_time"] = Number(start_time);
-      modified_tp[0]["end_time"] = modified_tp[0]["tp"];
-      for (let i = 1; i < timestamps.length; i++) {
-        modified_tp[i]["start_time"] = modified_tp[i - 1]["tp"];
-        modified_tp[i]["end_time"] = modified_tp[i]["tp"];
-      }
-      for (let i = 1; i < timestamps.length; i++) {
-        delete modified_tp[i].tp;
-      }
-      delete modified_tp[0].tp;
+      //axios request to get next question
       try {
         const config = {
           headers: {
             Authorization: localStorage.getItem("user_token")
           }
         };
-        console.log(config);
+        //editing timestamps
+        let modified_tp = timestamps[nr - 1];
+        if (nr - 1 == 0) {
+          let start_time = localStorage.getItem("start_time");
+          modified_tp["start_time"] = Number(start_time);
+          modified_tp["end_time"] = modified_tp["tp"];
+        } else {
+          modified_tp["start_time"] = timestamps[nr - 1]["tp"];
+          modified_tp["end_time"] = modified_tp["tp"];
+        }
+        delete modified_tp.tp;
         const res = await axios.post(
-          "http://localhost:5000/api/student-response",
-          { timestamps: modified_tp },
+          "http://localhost:5000/api/question/reqQuestion",
+          {
+            question_no: nr,
+            question_response: {
+              ...modified_tp,
+              student_response: responses[nr - 1]
+            },
+            student_response_id: student_response_id
+          },
           config
         );
         console.log(res.status);
-        localStorage.removeItem("start_time");
+        //add question to data array
+        let newData = data;
+        newData.append(res.data.random_question);
+        setData(newData);
       } catch (error) {
         console.log(error);
       }
+      pushData(nr);
+      setShowButton(false);
+      setQA(false);
+    } else {
+      //if all questions are answered
+      // let modified_tp = timestamps;
+      // let start_time = await localStorage.getItem("start_time");
+      // modified_tp[0]["start_time"] = Number(start_time);
+      // modified_tp[0]["end_time"] = modified_tp[0]["tp"];
+      // for (let i = 1; i < timestamps.length; i++) {
+      //   modified_tp[i]["start_time"] = modified_tp[i - 1]["tp"];
+      //   modified_tp[i]["end_time"] = modified_tp[i]["tp"];
+      // }
+      // for (let i = 1; i < timestamps.length; i++) {
+      //   delete modified_tp[i].tp;
+      // }
+      // delete modified_tp[0].tp;
+      // try {
+      //   const config = {
+      //     headers: {
+      //       Authorization: localStorage.getItem("user_token")
+      //     }
+      //   };
+      //   console.log(config);
+      //   const res = await axios.post(
+      //     "http://localhost:5000/api/reqQuestion",
+      //     { timestamps: modified_tp },
+      //     config
+      //   );
+      //   console.log(res.status);
+      //   localStorage.removeItem("start_time");
+      // } catch (error) {
+      //   console.log(error);
+      // }
     }
   };
 
@@ -110,14 +171,14 @@ const ViewQuestion = () => {
   };
 
   const pushData = nr => {
-    setQuestion(data[nr].question);
+    setQuestion(data[nr].description);
     setAnswer([
-      data[nr].answers[0],
-      data[nr].answers[1],
-      data[nr].answers[2],
-      data[nr].answers[3]
+      data[nr].alternatives[0],
+      data[nr].alternatives[1],
+      data[nr].alternatives[2],
+      data[nr].alternatives[3]
     ]);
-    setQid(data[nr].qid);
+    setQid(data[nr]._id);
     setCorrect(data[nr].correct);
     setNr(nr + 1);
   };
