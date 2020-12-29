baf78e04c461090ca39b8547abc7379382c39036
scyther67 <scyther67@gmail.com>
2020-12-29 23:09:55 +0530

    student-response schema changed and related dbfunctions altered

--- backend/api/dbFunctions/student_response.js
+++ backend/api/dbFunctions/student_response.js
@@ -1,13 +1,24 @@
 const Student_Response = require("../models/studentresponse");
 
 module.exports = {
-    createStudentResponse: async (student_id, response) => {
-        let studentresponse = new Student_Response({
-            student_id,
-            response
+    addStudentResponse: async ( user_response, start_time, end_time, question_ref, answer_correct, user_id ) => {
+        let student_response = new Student_Response({
+            user_response, 
+            start_time, 
+            answer_correct,
+            end_time, 
+            question_ref, 
+            user_id
         });
-        return studentresponse.save();
+        return student_response.save();
     },
+    // createStudentResponse: async (user_id, response) => {
+    //     let studentresponse = new Student_Response({
+    //         user_id,
+    //         response
+    //     });
+    //     return studentresponse.save();
+    // },
     findStudentResponseById: async (_id) => {
         return Student_Response.findById(_id);
     },
--- backend/api/routes/student_response/response.js
+++ backend/api/routes/student_response/response.js
@@ -7,12 +7,7 @@ module.exports = async (req, res) => {
   try {
     const response = await createStudentResponse(req.body.userId, req.body.timestamps);
     if (response == null) return res.json({...ServerError });
-    // const response = await Student_Response.create({
-    //   student_id: req.body.userId,
-    //   response: req.body.timestamps,
-    // });
-    // console.log(response);
-
+    
     return res.json({ ...Success });
   } catch (error) {
     console.log(error);
